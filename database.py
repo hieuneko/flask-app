13f8eeec84ab4b65f7d8d82c2e31e5a80bd3e4f9
hieuneko <hieuneko@gmail.com>
2021-12-14 21:11:21 +0700

    ok

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-import sqlite3
+# import sqlite3
 
-conn = sqlite3.connect('database.db');
-conn.execute('CREATE TABLE user (name TEXT, email TEXT, phone TEXT, pass TEXT , dateOfBirth TEXT)');
-conn.close()
+# conn = sqlite3.connect('database.db');
+# conn.execute('CREATE TABLE user (name TEXT, email TEXT, phone TEXT, pass TEXT , dateOfBirth TEXT)');
+# conn.close()
